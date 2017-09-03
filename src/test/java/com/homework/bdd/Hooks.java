9cd4356cdca81f41642246245035112c4c0be882
Nirmitaparekh <nirmitaparekh27@gmail.com>
2017-09-03 14:49:09 +0100

    something

--- src/test/java/com/homework/bdd/Hooks.java
+++ src/test/java/com/homework/bdd/Hooks.java
@@ -4,9 +4,6 @@ package com.homework.bdd;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
-/**
- * Created by Kajal on 03/09/2017.
- */
 public class Hooks {
     DriverManager driverManager = new DriverManager();
 
