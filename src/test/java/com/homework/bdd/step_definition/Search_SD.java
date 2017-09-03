6b41038d57fc1941134c3648e79c668275c34403
mshrimanker <you@example.com>
2017-09-03 14:17:50 +0100

    implemented selenium code for search step defin

--- src/test/java/com/homework/bdd/step_definition/Search_SD.java
+++ src/test/java/com/homework/bdd/step_definition/Search_SD.java
@@ -1,5 +1,6 @@
 package com.homework.bdd.step_definition;
 
+import com.homework.bdd.HomePage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -8,12 +9,15 @@ import cucumber.api.java.en.When;
  * Created by Kajal on 03/09/2017.
  */
 public class Search_SD {
+    HomePage homePage=new HomePage();
+
     @Given("^I am on homepage$")
     public void i_am_on_homepage(){
     }
 
     @When("^I search for \"([^\"]*)\"$")
     public void i_search_for(String arg1) {
+        homePage.doSearch();
     }
 
     @Then("^I should see respective \"([^\"]*)\" products\\.$")
