a1453f59e9e0ada709e93ba662a8e9efa4bac4a0
mshrimanker <you@example.com>
2017-09-04 17:03:15 +0100

    search commit

--- src/test/java/com/homework/bdd/step_definition/Search_SD.java
+++ src/test/java/com/homework/bdd/step_definition/Search_SD.java
@@ -10,15 +10,14 @@ import cucumber.api.java.en.When;
  * Created by Kajal on 03/09/2017.
  */
 public class Search_SD {
-    HomePage homePage=new HomePage();
-    ResultsPage resultsPage =new ResultsPage();
+    HomePage homePage = new HomePage();
+    ResultsPage resultsPage = new ResultsPage();
 
     @Given("^I am on homepage$")
-    public void i_am_on_homepage(){
+    public void i_am_on_homepage() {
         homePage.isOnHomePage();
 
     }
-
 
     @When("^I search for \"([^\"]*)\"$")
     public void i_search_for(String arg1) {
